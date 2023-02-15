81017ee5ac7104aba9d296c1ac71160eaed7e62c
Oliver <93136812+s214684@users.noreply.github.com>
2023-02-15 15:41:56 +0100

    Idea of program structure

---
+++ wifitool/module1/__main__.py
@@ -0,0 +1,13 @@
+# Get SSIDS in wifi medium (scan network)
+
+# Choose one SSID to emulate - e.g. based on WPA2, name, number of clients connected to it
+
+# Can we crack the key in order to create our own AP? if not go to next..
+
+# Create rogue AP with same SSID+key
+
+# Send beacon frames where we look like good AP (but we are the bad AP)
+
+# De-auth clients connected to SSID
+
+# Let clients connect to our rogue AP
