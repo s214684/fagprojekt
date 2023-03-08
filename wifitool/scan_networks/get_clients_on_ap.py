b26d8d1174b7ae722bac932e20e4ba544514f4a4
Nicklas <nicklaskiaer@gmail.com>
2023-03-08 16:09:27 +0100

    added FCfield

--- wifitool/scan_networks/get_clients_on_ap.py
+++ wifitool/scan_networks/get_clients_on_ap.py
@@ -7,12 +7,21 @@ def get_clients_on_ap(timeout: int, iface: str, dst_BSSID: str) -> list:
 
     def _callback(packet):
         if packet.haslayer(Dot11):
-            # extract the MAC address of the client
-            src_BSSID = packet[Dot11].addr2
 
-            # check if destination address is as specified
-            if packet[Dot11].addr1 == dst_BSSID:
-                client_list.append(src_BSSID)
+            # extract the to-DS/from-DS status of packet
+            DS = packet[Dot11].FCfield & 0x3
+            to_DS = DS & 0x1 != 0
+            from_DS = DS & 0x2 != 0
+
+            # check if sender is client or ap
+            if to_DS is True and from_DS is False:
+
+                # extract the MAC address of the client
+                src_BSSID = packet[Dot11].addr1
+
+                # check if destination address is as specified
+                if packet[Dot11].addr2 == dst_BSSID:
+                    client_list.append(src_BSSID)
 
     sniff(timeout=timeout, iface=iface, prn=_callback)
 
