9d433d715e1f4ec2843366ec9eecc8a29b62cbdd
Nicklas <nicklaskiaer@gmail.com>
2023-03-08 15:17:13 +0100

    updated get_clients_on_ap

--- wifitool/scan_networks/get_clients_on_ap.py
+++ wifitool/scan_networks/get_clients_on_ap.py
@@ -1,4 +1,4 @@
-from scapy.all import Dot11Beacon, Dot11, sniff
+from scapy.all import Dot11, sniff
 
 
 def get_clients_on_ap(timeout: int, iface: str, dst_BSSID: str) -> list:
@@ -6,12 +6,12 @@ def get_clients_on_ap(timeout: int, iface: str, dst_BSSID: str) -> list:
     client_list = []
 
     def _callback(packet):
-        if packet.haslayer(Dot11Beacon):
+        if packet.haslayer(Dot11):
             # extract the MAC address of the client
-            src_BSSID = packet[Dot11].addr1
+            src_BSSID = packet[Dot11].addr2
 
-            # check if destination address is specified
-            if packet[Dot11].addr2 == dst_BSSID:
+            # check if destination address is as specified
+            if packet[Dot11].addr1 == dst_BSSID:
                 client_list.append(src_BSSID)
 
     sniff(timeout=timeout, iface=iface, prn=_callback)
