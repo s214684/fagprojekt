e4ff9f9611331d1cc43a01bddcfbbd131ceaed9a
Nicklas <nicklaskiaer@gmail.com>
2023-03-08 15:03:39 +0100

    .

--- wifitool/scan_networks/get_clients_on_ap.py
+++ wifitool/scan_networks/get_clients_on_ap.py
@@ -9,6 +9,8 @@ def get_clients_on_ap(timeout: int, iface: str, dst_BSSID: str) -> list:
         if packet.haslayer(Dot11Beacon):
             # extract the MAC address of the client
             src_BSSID = packet[Dot11].addr1
+
+            # check if destination address is specified
             if packet[Dot11].addr2 == dst_BSSID:
                 client_list.append(src_BSSID)
 
