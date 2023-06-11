67ea488eb51eee30efa2d9791230d0550fea5397
Oliver <93136812+s214684@users.noreply.github.com>
2023-06-11 16:41:44 +0200

    maybe?

--- wifitool/scan_networks/__main__.py
+++ wifitool/scan_networks/__main__.py
@@ -1,73 +1,26 @@
-from cli import prompt_menu, options
+from cli import prompt_menu, options_menu
 from scanner import Scanner
 from utils import check_system
 from utils import LOGGER
 import sys
 
 
-INTERFACE = check_system()
-TIMEOUT = 20
+def main(interface: str, timeout: int) -> None:
+    """Main function of the program."""
+    with Scanner(interface, timeout) as scanner:
+        """The context manager is entered here. Then the functions are called based on the user input."""
+        scanner = scanner
+        LOGGER.info("Starting WifiTool...")
+        # Clear screen
+        print("\033c")
+        try:
+            prompt_menu(scanner)
+        except KeyboardInterrupt:
+            print("\nExiting...")
+            sys.exit(0)
 
 
-with Scanner(INTERFACE, TIMEOUT) as scanner:
-    """The context manager is entered here. Then the functions are called based on the user input."""
-    scanner = scanner
-    LOGGER.info("Starting WifiTool...")
-    # Clear screen
-    print("\033c")
-    start = True
-    try:
-        action = prompt_menu(welcome=True, start=True)
-        while True:
-            # Clear screen
-            start = False
-            if action == "a.1":
-                LOGGER.info("Starting network scan...")
-                scanner.scan_network()
-                LOGGER.info("Network scan complete.")
-            elif action == "a.2" or action == "b.2":
-                LOGGER.info("Starting client scan...")
-                scanner.show_clients()
-                LOGGER.info("Client scan complete.")
-            elif action == "b.1":
-                LOGGER.info("Starting AP showcase...")
-                scanner.show_aps()
-                LOGGER.info("AP showcase complete.")
-            elif action == "b.3":
-                LOGGER.info("Starting deauth attack...")
-                scanner.send_deauth()
-                LOGGER.info("Deauth attack complete.")
-            elif action == "b.4":
-                LOGGER.info("Starting deauth with beacon attack...")
-                scanner.send_deauth_with_beacon()
-                LOGGER.info("Deauth with beacon attack complete.")
-            elif action == "a.3":
-                LOGGER.info("Options menu opening.")
-                options(scanner)
-                LOGGER.info("Options menu closed.")
-                start = True
-            elif action == "b.5":
-                LOGGER.info("Starting IV scan...")
-                scanner.get_ivs()
-                LOGGER.info("IV scan complete.")
-            elif action == "b.6":
-                LOGGER.info("Starting beacon attack...")
-                scanner.send_beacon()
-            elif action == "b.7":
-                filename = input("Filename: ")
-                scanner.save_scan(filename=
-                                  filename if filename.endswith(".json") else filename + ".json"
-                                    )
-            elif action == "b.8":
-                LOGGER.info("going back to start...")
-                start = True
-            elif action == "a.9" or action == "b.9":
-                LOGGER.info("Exiting...")
-                break
-            else:
-                print("Invalid input. Try again..")
-                LOGGER.info("Invalid input detected")
-            action = prompt_menu(start=start)
-    except KeyboardInterrupt:
-        print("\nExiting...")
-        sys.exit(0)
+if __name__ == "__main__":
+    INTERFACE = check_system()
+    TIMEOUT = 20
+    main(INTERFACE, TIMEOUT)
--- wifitool/scan_networks/cli.py
+++ wifitool/scan_networks/cli.py
@@ -1,6 +1,114 @@
 from scanner import Scanner
 import datetime
+import sys
 
+from utils import LOGGER
+
+
+def start_menu(scanner: Scanner):
+    """Prints the start menu and send user to chosen menu."""
+    ASCII_banner = f"""
+        ░▒█░░▒█░▀█▀░▒█▀▀▀░▀█▀░░░▀▀█▀▀░▒█▀▀▀█░▒█▀▀▀█░▒█░░░
+        ░▒█▒█▒█░▒█░░▒█▀▀░░▒█░░░░░▒█░░░▒█░░▒█░▒█░░▒█░▒█░░░
+        ░▒▀▄▀▄▀░▄█▄░▒█░░░░▄█▄░░░░▒█░░░▒█▄▄▄█░▒█▄▄▄█░▒█▄▄█
+        Time: {datetime.datetime.now()}
+        By: Lucas, Nicklas & Oliver :)
+
+        Welcome to WifiTool!
+        """
+    print(ASCII_banner)
+    print("This tool is made in order to examine network topology and possibly exploit deauthentication frames and WEP.")
+    print("The tool is made for educational purposes only and should not be used for illegal activities.")
+
+    print("""
+        Please choose what you want to do:
+
+        1. Scan network (obtain topology)
+        2. Crack WEP
+        3. Options
+
+        9. Exit (Ctrl+C)    
+    """)
+
+    action = input("Input action wanted: ").strip()
+
+    if action == "1":
+        scan_menu(scanner)
+    elif action == "2":
+        crack_menu(scanner)
+    elif action == "3":
+        options_menu(scanner)
+    elif action == "9":
+        print("Exiting...")
+        sys.exit(0)
+    else:
+        print("Invalid input. Try again..")
+        LOGGER.info("Invalid input detected")
+    start_menu(scanner)
+
+
+def scan_menu(scanner: Scanner):
+    """Prints the scan menu and send user to chosen action."""
+
+    print("""
+        Please choose what you want to do:
+    1. Scan network
+    2. Show APs
+    3. Show clients
+    4. Send deauth
+    5. Send deauth with beacon
+    6. Send beacon
+    7. Save scan
+
+    9. Back
+    """)
+    action = input("Input action wanted: ").strip()
+
+    if action == "1":
+        LOGGER.info("Starting network scan...")
+        scanner.scan_network()
+        LOGGER.info("Network scan complete.")
+    elif action == "2":
+        LOGGER.info("Starting AP showcase...")
+        scanner.show_aps()
+        LOGGER.info("AP showcase complete.")
+    elif action == "3":
+        LOGGER.info("Starting client showcase...")
+        scanner.show_clients()
+        LOGGER.info("Client showcase complete.")
+    elif action == "4":
+        LOGGER.info("Starting deauth...")
+        scanner.send_deauth()
+        LOGGER.info("Deauth complete.")
+    elif action == "5":
+        LOGGER.info("Starting deauth with beacon attack...")
+        scanner.send_deauth_with_beacon()
+        LOGGER.info("Deauth with beacon attack complete.")
+    elif action == "6":
+        LOGGER.info("Starting beacon attack...")
+        scanner.send_beacon()
+        LOGGER.info("Beacon attack complete.")
+    elif action == "7":
+        LOGGER.info("Saving scan...")
+        filename = input("Filename: ")
+        scanner.save_scan(filename=
+                            filename if filename.endswith(".json") else filename + ".json"
+                            )
+        LOGGER.info("Scan saved.")
+    elif action == "9":
+        start_menu(scanner)
+    else:
+        print("Invalid input. Try again..")
+        LOGGER.info("Invalid input detected")
+
+    scan_menu(scanner)
+
+
+def crack_menu(scanner: Scanner):
+    LOGGER.info("Starting IV scan...")
+    scanner.get_ivs()
+    LOGGER.info("IV scan complete.")
+    start_menu(scanner)
 
 # Terminal menu for user to choose what to do
 def prompt_menu(welcome: bool = False, start: bool = False):
@@ -57,7 +165,7 @@ def prompt_menu(welcome: bool = False, start: bool = False):
     return action
 
 
-def options(scanner: Scanner) -> None:
+def options_menu(scanner: Scanner) -> None:
     """Lets the user change the settings of the network scanner."""
     print("Current settings:")
     print(f"Timeout: {scanner.timeout}")
@@ -70,13 +178,12 @@ def options(scanner: Scanner) -> None:
     if choice == "1":
         timeout = int(input("Input new timeout: "))
         scanner.timeout = timeout
-        options(scanner)
     elif choice == "2":
         interface = input("Input new interface: ")
         scanner.interface = interface
-        options(scanner)
     elif choice == "3":
         return
     else:
         print("Invalid input. Try again..")
-        options(scanner)
+    options_menu(scanner)
+    
